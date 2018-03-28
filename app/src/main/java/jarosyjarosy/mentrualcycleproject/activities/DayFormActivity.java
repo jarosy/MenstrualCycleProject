83aebfb56603ded9693c026c7ea56dd37ee1a542
jarosy <jarosyjarosy@gmail.com>
2018-03-28 23:17:35 +0200

    poczatek projektu kalendarzyka

---
+++ app/src/main/java/jarosyjarosy/mentrualcycleproject/activities/DayFormActivity.java
@@ -0,0 +1,56 @@
+package jarosyjarosy.mentrualcycleproject.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import jarosyjarosy.mentrualcycleproject.R;
+
+public class DayFormActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_form);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        return true;
+                    }
+                });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/jarosyjarosy/mentrualcycleproject/models/Day.java
@@ -0,0 +1,106 @@
+package jarosyjarosy.mentrualcycleproject.models;
+
+import java.util.Date;
+import java.util.List;
+
+public class Day {
+    private Date createDate;
+    private Integer dayOfCycle;
+    private Float temperature;
+    private String bleeding;
+    private List<String> mucus;
+    private String dilationOfCervix;
+    private String hardnessOfCervix;
+    private Boolean ovulatoryPain;
+    private Boolean tensionInTheBreasts;
+    private String otherSymptoms;
+    private Boolean intercourse;
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Integer getDayOfCycle() {
+        return dayOfCycle;
+    }
+
+    public void setDayOfCycle(Integer dayOfCycle) {
+        this.dayOfCycle = dayOfCycle;
+    }
+
+    public Float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Float temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getBleeding() {
+        return bleeding;
+    }
+
+    public void setBleeding(String bleeding) {
+        this.bleeding = bleeding;
+    }
+
+    public List<String> getMucus() {
+        return mucus;
+    }
+
+    public void setMucus(List<String> mucus) {
+        this.mucus = mucus;
+    }
+
+    public String getDilationOfCervix() {
+        return dilationOfCervix;
+    }
+
+    public void setDilationOfCervix(String dilationOfCervix) {
+        this.dilationOfCervix = dilationOfCervix;
+    }
+
+    public String getHardnessOfCervix() {
+        return hardnessOfCervix;
+    }
+
+    public void setHardnessOfCervix(String hardnessOfCervix) {
+        this.hardnessOfCervix = hardnessOfCervix;
+    }
+
+    public Boolean getOvulatoryPain() {
+        return ovulatoryPain;
+    }
+
+    public void setOvulatoryPain(Boolean ovulatoryPain) {
+        this.ovulatoryPain = ovulatoryPain;
+    }
+
+    public Boolean getTensionInTheBreasts() {
+        return tensionInTheBreasts;
+    }
+
+    public void setTensionInTheBreasts(Boolean tensionInTheBreasts) {
+        this.tensionInTheBreasts = tensionInTheBreasts;
+    }
+
+    public String getOtherSymptoms() {
+        return otherSymptoms;
+    }
+
+    public void setOtherSymptoms(String otherSymptoms) {
+        this.otherSymptoms = otherSymptoms;
+    }
+
+    public Boolean getIntercourse() {
+        return intercourse;
+    }
+
+    public void setIntercourse(Boolean intercourse) {
+        this.intercourse = intercourse;
+    }
+}
